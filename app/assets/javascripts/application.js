68d79af6870e050c8e2b72341f157c6ded22e8b1
Xavier Denis <xldenis@gmail.com>
2014-04-05 06:37:28 -0400

    Initial selfie functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,97 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(function(){
+  var video = document.querySelector('video');
+  var canvas = document.querySelector('canvas');
+  var img = document.querySelector('img');
+  var target = document.querySelector('#post_image');
+  var ctx = canvas.getContext('2d');
+  var localMediaStream = null;
+
+  function snapshot() {
+    if (localMediaStream) {
+      ctx.drawImage(video, 0, 0);
+      // "image/webp" works in Chrome.
+      // Other browsers will fall back to image/png.
+      document.querySelector('img').src = canvas.toDataURL('image/webp');
+      canvas.toBlob(function(b){
+        data = new FormData();
+        data.append('post[image]',b,'image');
+        // data.append('post[content]',$('#post_content')[0].value);
+        data.append('post[content]',"lololol");
+        $.ajax({
+          url: $('form').attr('action'),
+          contentType: false,
+          type: 'POST',
+          dataType: 'json',
+          data: data,
+          processData: false
+        });
+
+      },'image/png');
+    }
+  }
+  function sizeCanvas(){
+  setTimeout(function() {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    img.height = video.videoHeight;
+    img.width = video.videoWidth;
+  }, 100);
+}
+  video.addEventListener('click', snapshot, false);
+
+  // Not showing vendor prefixes or code that works cross-browser.
+  navigator.webkitGetUserMedia({video: true}, function(stream) {
+    video.src = window.URL.createObjectURL(stream);
+    sizeCanvas();
+    localMediaStream = stream;
+  }, function(){});
+});
+function doRekog(imgdata) {
+  console.log("doRekog");
+  
+  // Init data
+  var formData = new FormData(document.forms[0]);
+  formData.append("uploaded_file",imgdata);
+  formData.append("api_key", '3LE8QixSwo0eHyCI');
+  formData.append("api_secret", 'aPQtDeGB7aPsPsyu');
+  formData.append("jobs", "scene");
+
+  // Show status
+  $("#statusText").html("Sending picture to rekognition.com...");
+
+  // Init and send request
+  $.ajax({
+    url: 'http://rekognition.com/func/api/',
+    data: formData,
+    cache: false,
+    contentType: false,
+    processData: false,
+    dataType:"json",
+    type: 'POST',
+    success: function (data) {
+
+      var result = "<h2>Results</h2>";
+
+      if(data.scene_understanding) {
+
+        var isFacePresent = false;
+
+        for(var i=0, len=data.scene_understanding.length; i<len; i++) {
+          var resultItem = data.scene_understanding[i];
+          result += resultItem.label + " (Confidence: "+resultItem.score+")<br/>";
+        }
+
+        if (resultItem.score > 0.85) // face present
+          document.forms[0].submit();
+        else
+          alert('Selfies or Roomies Only!!');
+      }
+      $("#statusText").html(result);
+    }
+  });
+}
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  before_filter do
+  resource = controller_name.singularize.to_sym
+  method = "#{resource}_params"
+  params[resource] &&= send(method) if respond_to?(method, true)
+end
+
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
   def current_user
