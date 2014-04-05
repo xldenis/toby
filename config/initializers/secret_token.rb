9599bee53f9a9fe7fe650f147cb1291dbff2c684
Xavier Denis <xldenis@gmail.com>
2014-04-04 23:05:39 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toby::Application.config.secret_key_base = '414b10d863b81bbffad48d3512a3b3fd9b40e0aa8cacd54af32a753b36cab0886c1000df4d10098413048bb8630925667876a491efd1d29b383012a81fcb5777'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Toby::Application.config.session_store :cookie_store, key: '_toby_session'
