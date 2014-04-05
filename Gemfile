5b3281c28e7360fd6387032d4f3ee3e13bf27114
Xavier Denis <xldenis@gmail.com>
2014-04-05 00:09:55 -0400

    Add login.

--- Gemfile
+++ Gemfile
@@ -18,7 +18,15 @@ gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+gem 'omniauth'
+
+gem 'omniauth-facebook'
+
+gem 'high_voltage'
+
+gem 'haml'
 # Use jquery as the JavaScript library
+
 gem 'jquery-rails'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+  def current_user
+    @current_user ||= User.where(id: session[:current_user_id]).first
+  end
+  def logged_in?
+   current_user.present?
+ end
+ def authorize
+  if not logged_in?
+    redirect_to "/login"
+  end
+end
 end
