2255711eb865b568b43d2eae3dc607e91f5c5de5
michael-golfi <michael.m.golfi@gmail.com>
2014-04-05 00:44:06 -0400

    Added API key and secret.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,67 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+ function emitStream() {
+	// Asking permission to get the user media.
+	// If permission granted, assign the stream to the HTML 5 video element.
+	navigator.webkitGetUserMedia({video: true, audio: true}, function (stream) {
+	that._video = document.querySelector('video');
+	that._video.src = window.URL.createObjectURL(stream);
+	});
+}
+
+
+ function takePicture() {
+	// Assigning the video stream to the canvas to create a picture.
+	that._canvas = document.querySelector('canvas');
+	var context = that._canvas.getContext('2d');
+	context.clearRect(0, 0, 0, 0);
+	context.drawImage(that._video, 0, 0, 400, 300);
+
+	doRekog(that._video);
+}
+
+function doRekog(imgdata) {
+	console.log("doRekog");
+	
+	// Init data
+	var formData = new FormData();
+	formData.append("uploaded_file",imgdata);
+	formData.append("api_key", '3LE8QixSwo0eHyCI');
+	formData.append("api_secret", 'aPQtDeGB7aPsPsyu');
+	formData.append("jobs", "scene");
+
+	// Show status
+	$("#statusText").html("Sending picture to rekognition.com...");
+	 
+	// Init and send request
+	$.ajax({
+	url: 'http://rekognition.com/func/api/',
+	data: formData,
+	cache: false,
+	contentType: false,
+	processData: false,
+	dataType:"json",
+	type: 'POST',
+		success: function (data) {
+
+			var result = "<h2>Results</h2>";
+
+			if(data.scene_understanding) {
+				
+				var isFacePresent = false;
+
+				for(var i=0, len=data.scene_understanding.length; i<len; i++) {
+					var resultItem = data.scene_understanding[i];
+					result += resultItem.label + " (Confidence: "+resultItem.score+")<br/>";
+				}
+
+				if (resultItem.score > 0.85) // face present
+					document.forms[0].submit();
+				else
+					alert('Selfies or Roomies Only!!');
+			}
+			$("#statusText").html(result);
+		}
+	});
+}
