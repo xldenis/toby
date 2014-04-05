aee06b3f031698cc56f178bbca427f97e0b29aa9
michael-golfi <michael.m.golfi@gmail.com>
2014-04-05 04:30:05 -0400

    Revised the application.js file and html posts to take a pic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,80 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+function hasGetUserMedia() {
+  	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+	if (hasGetUserMedia()) 
+	{
+	  // Good to go!
+	} else {
+	  alert('getUserMedia() is not supported in your browser');
+	}
+
+	var errorCallback = function(e) {
+	    console.log('Reeeejected!', e);
+	  };
+
+	  var pic = document.querySelector('camera');
+
+	  if (navigator.getUserMedia){
+
+		navigator.getUserMedia({camera:true}, 	    
+	    
+		function(stream){
+	    	
+	    	pic.src = window.URL.createObjectURL(localMediaStream);
+	    	doRekog(pic);
+
+		}, errorCallback);
+	}else{
+		pic.src = '';
+	}
+	 
+	}
+
+function doRekog(imgdata) {
+	console.log("doRekog");
+	
+	// Init data
+	var formData = new FormData();
+	formData.append("uploaded_file",imgdata);
+	formData.append("api_key", '3LE8QixSwo0eHyCI');
+	formData.append("api_secret", 'aPQtDeGB7aPsPsyu');
+	formData.append("jobs", "scene");
+
+	// Show status
+	$("#statusText").html("Sending picture to rekognition.com...");
+	 
+	// Init and send request
+	$.ajax({
+	url: 'http://rekognition.com/func/api/',
+	data: formData,
+	cache: false,
+	contentType: false,
+	processData: false,
+	dataType:"json",
+	type: 'POST',
+		success: function (data) {
+
+			var result = "<h2>Results</h2>";
+
+			if(data.scene_understanding) {
+				
+				var isFacePresent = false;
+
+				for(var i=0, len=data.scene_understanding.length; i<len; i++) {
+					var resultItem = data.scene_understanding[i];
+					result += resultItem.label + " (Confidence: "+resultItem.score+")<br/>";
+				}
+
+				if (resultItem.score > 0.85) // face present
+					document.forms[0].submit();
+				else
+					alert('Selfies or Roomies Only!!');
+			}
+			$("#statusText").html(result);
+		}
+	});
+}
