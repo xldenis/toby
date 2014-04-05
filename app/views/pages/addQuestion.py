d62ec0c670f35113e17da7a17a61ff6410c50fc5
mdamore <mdamore9@gmail.com>
2014-04-05 03:33:02 -0400

    Added some python scripts and css update

---
+++ app/views/pages/addQuestion.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# Import modules for CGI handling 
+import cgi, cgitb 
+
+# Create instance of FieldStorage 
+form = cgi.FieldStorage() 
+
+# Get data from fields
+surveyTitle = form.getvalue('survey_title')
+question  = form.getvalue('question')
+
+ssvFile =  open(survey.ssv, "w")
+
+ssvFile.write(question) 
+
+ssvFile.close()
